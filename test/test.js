e6b14e83dfb369377d8737cd44bd5c10e9d79b1f
Luis Hernan Paul <lhpaul11@gmail.com>
2012-11-11 21:46:01 -0300

    viendo si ahora funciona

--- test/test.js
+++ test/test.js
@@ -1,14 +1,20 @@
-var request = require('request'),
-      assert = require('assert');
-      port
+var vows = require('vows'),
+    assert = require('assert');
 
-      describe('Dispositivo de Luz', function(){
-      	describe('GET /status', function(){
-      		it("should respond with status 200", function(done){
-      			request('http://127.0.0.1:3000/status', function(err,resp,body){
-      				assert.equal(resp.statusCode, 200);
-      				done(); 
-            }); 
-        }); 
-    });
-});
+var api = {
+    get: function (path) {
+        return function () {
+            client.get(path, this.callback);
+        };
+    }
+};
+
+vows.describe('Accesing api').addBatch({
+  'GET /': { topic: function () {
+    client.get('/status', this.callback);
+  },
+  'should respond with a 200 OK': function (e, res) {
+    assert.equal (res.status, 200);
+  }
+}
+}).export(module);
