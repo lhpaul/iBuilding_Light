7a5fe457bdd912223477be40aeb3961505d406b8
sivicencio <sivicencio@gmail.com>
2012-11-12 23:06:31 -0300

    Subo cambios para mostrar información del dispositivo

--- disp_test_light.js
+++ disp_test_light.js
@@ -31,19 +31,47 @@ http.createServer(function (req, res) {
 		isOn = !isOn;
 		res.end('Switch!');
 	}
+
+	function info(url, req, res) {
+        var jsonAns = {
+            "name" : "Luz",
+            "links" : [
+                {
+                    "url" : "http://"+req.headers.host+"/",
+                    "label" : "Root",
+                    "description" : "Home dispositivo de Luz"
+                } ,
+                {
+                    "url" : "http://"+req.headers.host+"/status",
+                    "label" : "Status",
+                    "description" : "Ver el estado del interruptor de luz"
+                } , 
+                {
+                    "url" : "http://"+req.headers.host+"/toggle",
+                    "label" : "Toggle",
+                    "description" : "Presionar el interruptor de luz"
+                }
+            ]
+        }
+
+        res.end(JSON.stringify(jsonAns));
+    }
  
     switch(url_parts.pathname) {
-    case '/':
-	display_root(url_parts.pathname, req, res);
-	break;
-    case '/status':
-	getStatus(url_parts.pathname, req, res);
-	break;
-    case '/toggle':
-	switchStatus(url_parts.pathname, req, res);
-	break;
-    default:
-	display_404(url_parts.pathname, req, res);
+	    case '/':
+			display_root(url_parts.pathname, req, res);
+			break;
+	    case '/status':
+			getStatus(url_parts.pathname, req, res);
+			break;
+	    case '/toggle':
+			switchStatus(url_parts.pathname, req, res);
+			break;
+		case '/info':
+			info(url_parts.pathname, req, res);
+			break;
+	    default:
+			display_404(url_parts.pathname, req, res);
     }
     return;
  
@@ -51,8 +79,8 @@ http.createServer(function (req, res) {
      * Display the document root
      **/
     function display_root(url, req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('Dispositivo Luz: 1) /status  2) /toggle');
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('Dispositivo Luz: 1) /status  2) /toggle');
     }
  
     /**
